49d697217a621b2849eb080f9cfe4c79b4f2419d
Hannes Ljungberg <hannes@5monkeys.se>
2019-10-01 22:44:56 +0200

    Add documentation

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const core = require("@actions/core");
 const { action } = require("./src/action");
 
 action().catch(error => {
+  // Action threw an error. Fail the action with the error message.
   core.setFailed(error.message);
 });
